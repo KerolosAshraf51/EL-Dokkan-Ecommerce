4a6c69afc3de840ce294b28688f67885c6338b74
NourhanMohmedFaisel <mnourhan527@gmail.com>
2024-09-11 11:44:23 +0300

    add new push cart repo

---
+++ E-Commerce_API_Angular_Project/Interfaces/ICartRepo.cs
@@ -0,0 +1,15 @@
+﻿using E_Commerce_API_Angular_Project.Models;
+
+namespace E_Commerce_API_Angular_Project.Interfaces
+{
+    public interface ICartRepo
+    {
+        public void Add(Cart cart);
+        public void Update(Cart cart);
+        public void Delete(int id);
+        public List<Cart> GetAll();
+        public Cart GetById(int id);
+
+        public void Save();
+    }
+}
