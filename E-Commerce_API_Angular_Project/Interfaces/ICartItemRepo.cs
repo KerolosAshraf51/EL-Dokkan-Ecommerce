52573bfa89687c02c74e5ad7116a51a2684cce5e
NourhanMohmedFaisel <mnourhan527@gmail.com>
2024-09-11 11:53:33 +0300

    new push ICartItemRepo

---
+++ E-Commerce_API_Angular_Project/Interfaces/ICartItemRepo.cs
@@ -0,0 +1,15 @@
+﻿using E_Commerce_API_Angular_Project.Models;
+
+namespace E_Commerce_API_Angular_Project.Interfaces
+{
+    public interface ICartItemRepo
+    {
+        public void Add(CartItem cartItem);
+        public void Update(CartItem cartItem);
+        public void Delete(int id);
+        public List<CartItem> GetAll();
+        public CartItem GetById(int id);
+
+        public void Save();
+    }
+}
