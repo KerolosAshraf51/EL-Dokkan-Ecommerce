d6e5c3a759326a9bf0a55dbd8bb7202162f59fa5
Peter Kameel <peterkameel.93@gmail.com>
2024-09-14 13:34:37 +0300

    implement IReview and it's Repo

--- E-Commerce_API_Angular_Project/Interfaces/IReviewRepo.cs
+++ E-Commerce_API_Angular_Project/Interfaces/IReviewRepo.cs
@@ -1,6 +1,13 @@
-﻿namespace E_Commerce_API_Angular_Project.Interfaces
+﻿using E_Commerce_API_Angular_Project.Models;
+
+namespace E_Commerce_API_Angular_Project.Interfaces
 {
     public interface IReviewRepo
     {
+        public void Add(Review review);
+        public void Delete(int id);
+        public List<Review> GetAll();
+        public Review GetById(int id);
+        public void Save();
     }
 }
--- E-Commerce_API_Angular_Project/Repository/ReviewRepo.cs
+++ E-Commerce_API_Angular_Project/Repository/ReviewRepo.cs
@@ -1,6 +1,41 @@
-﻿namespace E_Commerce_API_Angular_Project.Repository
+﻿using E_Commerce_API_Angular_Project.Interfaces;
+using E_Commerce_API_Angular_Project.Models;
+
+namespace E_Commerce_API_Angular_Project.Repository
 {
-    public class ReviewRepo
+    public class ReviewRepo : IReviewRepo
     {
+        public EcommContext context { get; }
+        public ReviewRepo(EcommContext _context)
+        {
+            context = _context;
+        }
+
+
+        public void Add(Review review)
+        {
+            context.Add(review);
+        }
+
+        public void Delete(int id)
+        {
+            Review review = GetById(id);
+            context.Remove(review);
+        }
+
+        public List<Review> GetAll()
+        {
+            return context.Reviews.ToList();
+        }
+
+        public Review GetById(int id)
+        {
+            return context.Reviews.FirstOrDefault(r => r.Id == id);
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
     }
 }
