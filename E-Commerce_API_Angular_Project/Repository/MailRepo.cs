5d1f55f321f794ba3b5d9428fc538185a022e140
peter-mahrous-aziz <petermahrousaziz@gmail.com>
2024-09-27 18:57:24 +0200

    mail mantainance step 1

--- E-Commerce_API_Angular_Project/Repository/MailRepo.cs
+++ E-Commerce_API_Angular_Project/Repository/MailRepo.cs
@@ -3,6 +3,8 @@ using MailKit.Security;
 using System;
 using MailKit.Net.Smtp;
 using MimeKit;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http.HttpResults;
 
 
 
@@ -10,30 +12,43 @@ namespace E_Commerce_API_Angular_Project.Repository
 {
     public class MailRepo : IMailRepo
     {
-        public async void SendEmail(string toAddress, string subject, string body)
+        public async Task<string> SendEmail(string toAddress, string subject, string body)
 
         {
             var message = new MimeMessage();
 
-            message.From.Add(new MailboxAddress("EL-DOKAN", "eldokanonlinestore@hotmail.com"));
+            message.From.Add(new MailboxAddress("EL-DOKAN", "eldokanonlinestore@outlook.com"));
             message.To.Add(new MailboxAddress("", toAddress));
             message.Subject = subject;
 
             message.Body = new TextPart("html")
             {
                 Text = body
-            };  
-              
-            using (var client = new SmtpClient())
+            };
+
+            try
             {
-                await  client.ConnectAsync("smtp.office365.com", 587, SecureSocketOptions.StartTls);
-                client.Authenticate("eldokanonlinestore@hotmail.com", "dokan12345");
-                client.Send(message);
-                client.Disconnect(true);
+                using (var client = new SmtpClient())
+                {
+                    await client.ConnectAsync("smtp.outlook.com", 587, SecureSocketOptions.StartTls);
+                    client.Authenticate("eldokanonlinestore@outlook.com", "melkfqxsapfxprbz");
+                    client.Send(message);
+                    client.Disconnect(true);
+                }
+
+                return "1";
+
             }
+
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+
         }
 
 
-     
-}
+
+
+    }
 }
