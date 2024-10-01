421f7db2e7e989e86baf1c48c8f57644ce4748b6
peter-mahrous-aziz <petermahrousaziz@gmail.com>
2024-10-02 01:16:18 +0200

    IsBlocked?

--- E-Commerce_API_Angular_Project/Interfaces/IAppUserRepo.cs
+++ E-Commerce_API_Angular_Project/Interfaces/IAppUserRepo.cs
@@ -10,6 +10,8 @@ namespace E_Commerce_API_Angular_Project.IRepository
         public void Add(appUser user);
         public void Update(appUser user);
         public void Delete(appUser user);
+
+        public void Block(appUser user);
         public List<appUser> GetAll();
         public appUser GetById(int id);
         public void Save();
